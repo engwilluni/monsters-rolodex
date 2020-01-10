ccc5f1d42830613a241c62384606d801451bf0bd
will7seven <will7seven@gmail.com>
2020-01-10 10:15:40 -0300

    Updates

---
+++ precache-manifest.5add9003e9fa94119b5ddeaa007c58d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f50425b3bb960527965edb67ed5148",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1c13cfb1ab2d398ecd4",
+    "url": "/monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "a1c13cfb1ab2d398ecd4",
+    "url": "/monsters-rolodex/static/js/main.b3bfd6ca.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
